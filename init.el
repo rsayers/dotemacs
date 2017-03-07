c0d1bf4eef523f77912fa4a68a2b58f399c80e99
Rob Sayers <rsayers@robsayers.com>
2017-03-07 13:03:44 -0600

    Update init.el
    set up fci mode

--- init.el
+++ init.el
@@ -17,7 +17,9 @@
  '(custom-safe-themes
    (quote
     ("c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" default)))
- '(package-selected-packages (quote (magit monokai-theme elpy anaconda-mode))))
+ '(package-selected-packages
+   (quote
+    (fill-column-indicator magit monokai-theme elpy anaconda-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -41,3 +43,9 @@
 (menu-bar-mode -1)
 (setq-default indent-tabs-mode nil)
 (put 'downcase-region 'disabled nil)
+(require 'fill-column-indicator)
+(setq fci-rule-width 1)
+(setq fci-rule-use-dashes 1)
+(setq fci-rule-column 80)
+(setq fci-rule-color "darkblue")
+(add-hook 'after-change-major-mode-hook 'fci-mode)
